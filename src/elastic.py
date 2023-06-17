3d8cd1efdada3ca492dfa726b223a5e68e3285d2
Justin Guese <guese.justin@gmail.com>
2023-06-17 19:08:05 +0200

    adding openobserve functionality

--- src/elastic.py
+++ src/elastic.py
@@ -1,11 +1,15 @@
+import base64
+import json
 from datetime import datetime
 from os import environ
 
+import requests
 from elasticsearch import Elasticsearch
 
 MONTHDATE = datetime.now().strftime("%Y-%m")
 
 ES = None
+OO = None
 
 try:
     ES = Elasticsearch(environ.get("ELASTICSEARCH_URL", "http://localhost:9200"))
@@ -13,12 +17,36 @@ try:
 except Exception as e:
     print("Elastic Error: ", e)
     ES = None
+    
+    
+class OpenObserve:
+    def __init__(self, user, password) -> None:
+        bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
+        org = "tradingbot22"
+        openobserve_host = "http://openobserve.openobserve.svc.cluster.local:5080"
+        self.openobserve_url = openobserve_host + "/api/" + org + "/" + "[STREAM]" + "/_json"
+        self.headers =  {"Content-type": "application/json", "Authorization": "Basic " + bas64encoded_creds}
+
+    def index(self, index: str, document: dict):
+        res = requests.post(self.openobserve_url.replace("[STREAM]", index), headers=self.headers, data=json.dumps(document))
+        res.raise_for_status()
+        
+# openobserve support
+user = "root@example.com"
+password = "Complexpass#123"
+
+OO = OpenObserve(user, password)
 
 
 def logToElastic(index: str, document: dict):
-    global ES
+    global ES, OO
     if ES is not None:
         ES.index(index=index, body=document)
+    if OO is not None:
+        try:
+            OO.index(index, document)
+        except Exception as e:
+            print("skip - OpenObserve Error: ", str(e))
         
 def logError(module: str, stock: str, error: str, severity = "medium"):
     error = {
