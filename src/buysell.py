0dc54ce7487604eccca52556afe840e8625184a0
Justin Guese <guese.justin@gmail.com>
2023-08-02 18:34:50 +0200

    USD protection

--- src/buysell.py
+++ src/buysell.py
@@ -15,102 +15,11 @@ from pricing_functions import COMMISSION, __getCurrentPrice
 
 # alpacaInterface = AlpacaInterface()
 
-async def __liveBuy(bot, ticker, amount, short, currentPrice):
-    # this works for both long and shor
-    #### attention, is live!!!!
-    if amount < 0:
-        raise HTTPException(status_code=400, detail="Amount for live buy cant be negative")
-    try:
-        # alpacaInterface.buyLive(ticker, amount)
-        # sendToSlack(bot.name, f"just bought {round(amount * currentPrice,2)}$ of {ticker}", "info")
-        pass
-    except Exception as e:
-        body = {
-            "botName" : bot.name, 
-            "@timestamp" : datetime.utcnow(),
-            "ticker": ticker,
-            "buy": True,
-            "LIVE" : True,
-            "short": short,
-            "price": currentPrice,
-            "quantity": amount,
-            "error" : str(e)
-            }
-        logToElastic("tradingbot22_live_errors", body)
-        print("Alpaca Error while buying live: " + str(e))
-        sendToSlack(bot.name, "Alpaca 1. buy live denied: " + str(e) + " content: " + json.dumps(body), "info")
-    # most of the time error happens bc only int works
-    amount = int(amount)
-    if amount > 0:
-        try:
-            alpacaInterface.buyLive(ticker, amount)
-            sendToSlack(bot.name, f"just bought {round(amount * currentPrice,2)}$ of {ticker}", "info")
-        except Exception as e:
-            body = {
-                "botName" : bot.name, 
-                "@timestamp" : datetime.utcnow(),
-                "ticker": ticker,
-                "buy": True,
-                "LIVE" : True,
-                "short": short,
-                "price": currentPrice,
-                "quantity": amount,
-                "error" : str(e)
-                }
-            logToElastic("tradingbot22_live_errors", body)
-            sendToSlack(bot.name, "Alpaca 2. buy live error: " + str(e) + " content: " + json.dumps(body), "error")
-            raise HTTPException(status_code = 500, details="Alpaca Error while buying live: " + str(e))
-    return amount
-
-def __liveSell(bot, ticker, amount, short, currentPrice):
-    # this works for both long and shor
-    #### attention, is live!!!!
-    if amount < 0:
-        raise HTTPException(status_code=400, detail="Amount for live buy cant be negative")
-    try:
-        alpacaInterface.sellLive(ticker, amount)
-        sendToSlack(bot.name, f"just sold {round(amount * currentPrice,2)}$ of {ticker}", "info")
-    except Exception as e:
-        body = {
-            "botName" : bot.name, 
-            "@timestamp" : datetime.utcnow(),
-            "ticker": ticker,
-            "buy": True,
-            "LIVE" : True,
-            "short": short,
-            "price": currentPrice,
-            "quantity": amount,
-            "error" : str(e)
-            }
-        logToElastic("tradingbot22_live_errors", body)
-        print("Alpaca Error while buying live: " + str(e))
-        sendToSlack(bot.name, "Alpaca 1. sell live denied: " + str(e) + " content: " + json.dumps(body), "info")
-    # most of the time error happens bc only int works
-    amount = int(amount)
-    if amount > 0:
-        try:
-            alpacaInterface.sellLive(ticker, amount)
-            sendToSlack(bot.name, f"just sold {round(amount * currentPrice,2)}$ of {ticker}", "info")
-        except Exception as e:
-            body = {
-                "botName" : bot.name, 
-                "@timestamp" : datetime.utcnow(),
-                "ticker": ticker,
-                "buy": True,
-                "LIVE" : True,
-                "short": short,
-                "price": currentPrice,
-                "quantity": amount,
-                "error" : str(e)
-                }
-            logToElastic("tradingbot22_live_errors", body)
-            sendToSlack(bot.name, "Alpaca 2. sell live error: " + str(e) + " content: " + json.dumps(body), "error")
-            raise HTTPException(status_code = 500, details="Alpaca Error while selling live: " + str(e))
-    return amount
-
 async def __buy_stock(botname: str, ticker: str, 
         db: Session, amount: float = -1,
         amountInUSD: bool = False, short: bool = False):
+    if ticker == "USD":
+        raise HTTPException(status_code=400, detail="USD is not a stock")
     bot = db.query(Bot).filter(Bot.name == botname).first()
     if bot is None:
         raise HTTPException(status_code=404, detail="Bot not found")
@@ -139,12 +48,8 @@ async def __buy_stock(botname: str, ticker: str,
         elif bot.portfolio[ticker] < 0 and not short:
             # uhoh!
             raise HTTPException(status_code=400, detail="Already short on this stock. cant open long. sell first")
-        if bot.live:
-            amount = await __liveBuy(bot, ticker, amount, short, currentPrice)
         bot.portfolio[ticker] += amount
     else:
-        if bot.live:
-            amount = await __liveBuy(bot, ticker, amount, short, currentPrice)
         bot.portfolio[ticker] = amount
         
     # abs to support shorts and long
@@ -180,6 +85,8 @@ async def __buy_stock(botname: str, ticker: str,
 async def __sell_stock(botname: str, ticker: str, 
         db: Session, amount: float = -1,
         amountInUSD: bool = False, short: bool = False):
+    if ticker == "USD":
+        raise HTTPException(status_code=400, detail="USD is not a stock")
     bot = db.query(Bot).filter(Bot.name == botname).first()
     if bot is None:
         raise HTTPException(status_code=404, detail="Bot not found")
@@ -208,8 +115,6 @@ async def __sell_stock(botname: str, ticker: str,
     # add trade on bot
     if bot.portfolio[ticker] > 0:
         # long, is no short
-        if bot.live:
-            amount = await __liveSell(bot, ticker, amount, short, currentPrice)
         bot.portfolio[ticker] -= amount
         bot.portfolio["USD"] += amount * currentPrice * (1 - COMMISSION)
     elif bot.portfolio[ticker] < 0 and short:
@@ -217,8 +122,6 @@ async def __sell_stock(botname: str, ticker: str,
         # we need to load the price from the db to calculate the profit
         theBuyTrade = db.query(Trade).filter(Trade.bot == botname).filter(Trade.ticker == ticker).filter(Trade.buy == True).filter(Trade.short == True).order_by(Trade.id.desc()).first()
         diff = theBuyTrade.price - currentPrice
-        if bot.live:
-            amount = await __liveSell(bot, ticker, amount, short, currentPrice)
         bot.portfolio["USD"] += (theBuyTrade.price + diff) * abs(amount) * (1 - COMMISSION)
         bot.portfolio[ticker] += abs(amount)
     else:
