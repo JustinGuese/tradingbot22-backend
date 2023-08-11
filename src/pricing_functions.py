7a9f017e061bb1b7713911e6e7df567127642f48
Justin Guese <guese.justin@gmail.com>
2023-08-11 10:46:53 +0200

    adding even one more layer of get crnt price

--- src/pricing_functions.py
+++ src/pricing_functions.py
@@ -11,6 +11,8 @@ async def __getYFPrice(ticker: str) -> float:
     df = yf.download(ticker, interval = "1d", period="1d", progress=False)
     if len(df) == 0:
         df = yf.download(ticker, interval = "1d", period="5d", progress=False)
+    if len(df) == 0:
+        df = yf.download(ticker, interval = "1d", period="1mo", progress=False)
     assert len(df) > 0, "df is empty"
     return float(df["Close"].iloc[-1])
 
